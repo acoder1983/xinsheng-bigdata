a892f8abe2dc68b26a7a35faa2a3998e7d706ce6
acoder1983 <acoder1983@163.com>
2016-06-01 21:34:29 +0800

    import tests

---
+++ crawler/crawler/test/testItem.py
@@ -0,0 +1,7 @@
+import unittest
+# import crawler
+from crawler.items import CrawlerItem
+
+class TestItem(unittest.TestCase):
+    def testItem(self):
+        self.assertTrue(True)
---
+++ crawler/crawler/testMain.py
@@ -0,0 +1,10 @@
+import unittest
+
+from test.testItem import TestItem
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    for test_class in (TestItem):
+        tests = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(tests)
+    unittest.TextTestRunner().run(suite)
