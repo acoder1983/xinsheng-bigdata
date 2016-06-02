4acf43bf2adcfaa5122b5de63ea5e58daf009297
acoder1983 <acoder1983@163.com>
2016-06-02 19:35:54 +0800

    Update testItem.py

--- crawler/crawler/test/testItem.py
+++ crawler/crawler/test/testItem.py
@@ -1,7 +1,10 @@
 import unittest
-# import crawler
-from crawler.items import CrawlerItem
+import sys
+sys.path.insert(0, '..')
+from items import CrawlerItem
+
 
 class TestItem(unittest.TestCase):
+
     def testItem(self):
         self.assertTrue(True)
