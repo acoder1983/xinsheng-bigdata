ac23ad2ef7b635032bcd8f95cd6e4dc88b46d0fb
acoder1983 <acoder1983@163.com>
2016-06-02 19:42:11 +0800

    Update testMain.py

--- crawler/crawler/testMain.py
+++ crawler/crawler/testMain.py
@@ -1,10 +1,10 @@
 import unittest
 
-from test.testItem import TestItem
+from testItem import TestItem
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    for test_class in (TestItem):
-        tests = loader.loadTestsFromTestCase(test_class)
+    for test_class in (TestItem,):
+        tests = unittest.TestLoader().loadTestsFromTestCase(test_class)
         suite.addTests(tests)
     unittest.TextTestRunner().run(suite)
