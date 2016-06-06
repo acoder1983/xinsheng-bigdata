469038f6e685835db7eb11fb540ab7274368b000
acoder1983 <acoder1983@163.com>
2016-06-06 22:00:04 +0800

    update items

--- crawler/crawler/test/testItem.py
+++ crawler/crawler/test/testItem.py
@@ -2,13 +2,20 @@ import unittest
 import sys
 sys.path.insert(0, '..')
 from items import TopicItem
-
+from items import Member
 
 class TestItem(unittest.TestCase):
 
-    def testItem(self):
+    def testTopicItem(self):
         item1=TopicItem()
         item1['title']="a"
         item2=TopicItem()
         item2['title']="b"
-        self.assertTrue(item1['title'],item2['title'])
+        self.assertNotEqual(item1['title'],item2['title'])
+
+    def testMember(self):
+    	m1=Member()
+    	m1.name="a"
+    	m2=Member()
+    	m2.name="b"
+    	self.assertNotEqual(m1.name,m2.name)
