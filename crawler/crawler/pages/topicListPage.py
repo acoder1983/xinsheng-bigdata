997bfd63e989f6e6aeee74f5be3e06bdcab08da3
acoder1983 <acoder1983@163.com>
2016-06-10 17:41:23 +0800

    add tests

--- crawler/crawler/pages/topicListPage.py
+++ crawler/crawler/pages/topicListPage.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
+from bs4 import BeautifulSoup
 
 class TopicListPage(object):
 	def __init__(self, responseBody):
-		body=responseBody
+		self.soup=BeautifulSoup(responseBody,"html5lib")
+
 
 	def getTopicUrls(self):
-		return []
+		return len(self.soup.find_all('a'))
 
 	def getNextPageUrl(self):
 		pass
