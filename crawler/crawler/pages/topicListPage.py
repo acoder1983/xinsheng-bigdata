38a4e4c9df53b6b57ddf8ebed7e33245035e5574
acoder1983 <acoder1983@163.com>
2016-06-10 18:05:37 +0800

    add tests

--- crawler/crawler/pages/topicListPage.py
+++ crawler/crawler/pages/topicListPage.py
@@ -2,13 +2,19 @@
 from bs4 import BeautifulSoup
 
 class TopicListPage(object):
-	def __init__(self, responseBody):
-		self.soup=BeautifulSoup(responseBody,"html5lib")
+    def __init__(self, responseBody):
+        self.soup=BeautifulSoup(responseBody,"html5lib")
 
 
-	def getTopicUrls(self):
-		return len(self.soup.find_all('a'))
+    def getTopicUrls(self):
+        n=0
+        for link in self.soup.find_all('a'):
+            if link.has_attr('href'):
+                if link['href'].startswith('http://xinsheng.huawei.com/cn/index.php?app=forum&mod=Detail&act=index&id='):
+                    n+=1
+        print n
+        return len(self.soup.find_all('a'))
 
-	def getNextPageUrl(self):
-		pass
-		
+    def getNextPageUrl(self):
+        pass
+        
