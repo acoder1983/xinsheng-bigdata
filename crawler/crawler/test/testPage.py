95d0ba87735b7c757000458884f99f9025ac2078
acoder1983 <acoder1983@163.com>
2016-06-18 14:45:06 +0800

    Update testPage.py

--- crawler/crawler/test/testPage.py
+++ crawler/crawler/test/testPage.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import re
 import unittest
 import sys
 sys.path.insert(0, '../pages')
@@ -27,14 +28,43 @@ class TestTopicListPage(unittest.TestCase):
 
 class TestTopicPage(unittest.TestCase):
 
-    def setUp(self):
+    def testGetTopic1(self):
         f = open('data/topic1-1.html')
         self.body = f.read()
-
-    def testGetTopic(self):
         page = TopicPage(self.body)
         topic = page.getTopic()
 
         self.assertEqual("【枪林弹雨中成长】系列故事汇总贴，最新上映：《沸腾的战场》", topic['title'])
 
         self.assertEqual("华为家事", topic['author']['name'])
+        self.assertEqual(
+            "http://xinsheng.huawei.com/cn/index.php?app=space&mod=Index&act=view&maskId=115", topic['author']['url'])
+
+        self.assertEqual("2015-12-10 09:30", topic['time'])
+
+        self.assertEqual("51685", topic['reads'])
+
+    def testGetTopic2(self):
+        f = open('data/topic2-1.html')
+        self.body = f.read()
+        page = TopicPage(self.body)
+        topic = page.getTopic()
+
+        self.assertEqual("海外常驻，老婆在家，两地分居，怎么办？", topic['title'])
+
+        self.assertEqual("用心生活", topic['author']['name'])
+        self.assertEqual("javascript:void(0);", topic['author']['url'])
+
+        self.assertEqual("2008-07-02 12:42", topic['time'])
+
+        self.assertEqual("6471", topic['reads'])
+
+    def testSearchTime(self):
+        actual = TopicPage.extractTime(
+            '\xa0\u4e8e 2008-07-02 12:42\xa0\u53d1\u8868')
+        self.assertEqual('2008-07-02 12:42', actual)
+
+    def testRemoveAllBras(self):
+        actual = TopicPage.removeAllBras(
+            '<div>abc <span>123</span>\n<div>')
+        self.assertEqual('abc 123\n', actual)
