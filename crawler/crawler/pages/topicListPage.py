7b71cf5371c9b4767b2792ac93057b1cf90a1650
acoder1983 <acoder1983@163.com>
2016-06-18 14:43:16 +0800

    Update topicListPage.py

--- crawler/crawler/pages/topicListPage.py
+++ crawler/crawler/pages/topicListPage.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 from bs4 import BeautifulSoup
 
-class TopicListPage(object):
-    def __init__(self, responseBody):
-        self.body=responseBody
-        self.soup=BeautifulSoup(responseBody,"html5lib")
 
+class TopicListPage(object):
+
+    def __init__(self, responseBody):
+        self.body = responseBody
+        self.soup = BeautifulSoup(responseBody, "html5lib")
 
     def getTopicUrls(self):
-        urls=[]
+        urls = []
         for link in self.soup.find_all('a'):
             if link.has_attr('href'):
                 if link['href'].startswith('http://xinsheng.huawei.com/cn/index.php?app=forum&mod=Detail&act=index&id='):
@@ -16,17 +17,16 @@ class TopicListPage(object):
         return urls
 
     def getNextPageUrl(self):
-        key="changeToPage"
+        key = "changeToPage"
         try:
-            p=self.body.index(key)
-            key="href=\""
-            beg=self.body.index(key,p)
-            beg+=len(key)
-            key="\">"
-            end=self.body.index(key,beg)
-            url= "http://xinsheng.huawei.com"+self.body[beg:end]
-            return url.replace("amp;","")
+            p = self.body.index(key)
+            key = "href=\""
+            beg = self.body.index(key, p)
+            beg += len(key)
+            key = "\">"
+            end = self.body.index(key, beg)
+            url = "http://xinsheng.huawei.com" + self.body[beg:end]
+            return url.replace("amp;", "")
         except Exception, e:
             print e
             return None
-        
